15d7e8f147135ea7fa54dc5fbe0e72279ad573c6
Alexander Kostarev <UniverAlex@ya.ru>
2022-06-06 06:54:47 +0300

    Навигация на разделы

---
+++ js/router.js
@@ -0,0 +1,30 @@
+const route = (event) => {
+    event = event || window.event;
+    event.preventDefault();
+    window.history.pushState({}, "", event.target.href);
+    handleLocation();
+};
+
+document.addEventListener("DOMContentLoaded", () => {
+    document.querySelectorAll("header a").forEach(btn => btn.addEventListener("click", route));
+});
+
+const routes = {
+    404: "pages/main.html",
+    "/": "pages/main.html",
+    "/aerography": "pages/aerography.html",
+    "/walls": "pages/walls.html",
+    "/sculptures": "pages/sculptures.html",
+};
+
+const handleLocation = async () => {
+    const path = window.location.pathname;
+    const route = routes[path] || routes[404];
+    const html = await fetch(route).then((data) => data.text());
+    document.getElementById("main-page").innerHTML = html;
+};
+
+window.onpopstate = handleLocation;
+window.route = route;
+
+handleLocation();
