6c84150d7c0f150c072812216e3c98c66f5de26b
Alexander Kostarev <UniverAlex@ya.ru>
2022-06-11 08:12:44 +0300

    Роутинг: Испрпалена ошибка после обновления страницы

--- js/router.js
+++ js/router.js
@@ -12,14 +12,14 @@ document.addEventListener("DOMContentLoaded", () => {
 const routes = {
     404: "pages/main.html",
     "/": "pages/main.html",
-    "/aerography": "pages/aerography.html",
-    "/walls": "pages/walls.html",
-    "/sculptures": "pages/sculptures.html",
+    "#aerography": "pages/aerography.html",
+    "#walls": "pages/walls.html",
+    "#sculptures": "pages/sculptures.html"
 };
 
 const handleLocation = async () => {
-    const path = window.location.pathname;
-    const route = routes[path] || routes[404];
+    const hash = window.location.hash;
+    const route = routes[hash] || routes[404];
     const html = await fetch(route).then((data) => data.text());
     document.getElementById("main-page").innerHTML = html;
 };
