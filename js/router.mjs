b3362c30e7b4a69bf8160a952993d66cce387ed3
Alexander Kostarev <UniverAlex@ya.ru>
2022-10-28 10:39:13 +0300

    Выделяем цветом ссылку для главной страницы

---
+++ js/router.mjs
@@ -0,0 +1,67 @@
+function Router() {
+    document.addEventListener("DOMContentLoaded", () => {
+        document.querySelectorAll("header a").forEach((navLink) => {
+            navLink.addEventListener("click", routeFunc);
+        });
+
+        window.onpopstate = handleLocation.bind(this);
+        window.route = routeFunc;
+
+        handleLocation.bind(this)();
+
+        function routeFunc(event) {
+            event = event || window.event;
+            event.preventDefault();
+            window.history.pushState({}, "", event.target.href);
+            handleLocation.bind(this)();
+        }
+
+        function handleLocation() {
+            const hash = window.location.hash;
+
+            const routes = {
+                404: "pages/main.html",
+                "/": "pages/main.html",
+                "#aerography": "pages/aerography.html",
+                "#walls": "pages/walls.html",
+                "#sculptures": "pages/sculptures.html"
+            };
+
+            const route = routes[hash] || routes[404];
+
+            //  Выделяем выбранный раздел, чтобы было понятнее.
+            const categories = document.getElementsByClassName("category");
+            for (let i = 0; i < categories.length; i++) {
+                categories.item(i).classList.remove("selected");
+            }
+            getSelectedLink(hash).classList.add("selected");
+
+            // Запрашиваем страницу и вставляем в div content.
+            fetch(route).then((data) => {
+                data.text().then(html => {
+                    document.getElementById("content").innerHTML = html;
+                    if (this.callbackFunc) {
+                        this.callbackFunc(hash, html);
+                    }
+                });
+            });
+        };
+
+        function getSelectedLink(hash) {
+            const categoryMatch = hash.match(/#(\w*)/);
+            if (categoryMatch) {
+                const hashWithoutHashtag = categoryMatch[1];
+                return document.getElementById(hashWithoutHashtag + "Link");
+            } else {
+                return document.getElementById("mainPageLink");
+            }
+        }
+    });
+
+    return {
+        attachMatched: (callbackFunc) => {
+            this.callbackFunc = callbackFunc;
+        }
+    };
+}
+export default Router
--- js/script.mjs
+++ js/script.mjs
@@ -1,2 +1,11 @@
-import GalleryCard from "./components/GalleryCard.mjs";
-customElements.define("gallery-card", GalleryCard);
+import Router from "./router.mjs";
+const router = new Router();
+// TODO: Подготовь изображения по соотношения карточек. Как вариант сделать превьюхи отдельно.
+router.attachMatched((hash, html) => {
+    // TODO: с помощью селекторов навешивай логику, чтобы не подвязываться на конкретный хэш
+    if (hash === "#aerography") {
+        document.getElementById("rubinFolder").addEventListener("click", () => {
+            document.getElementById("rubinDialog").showModal();
+        });
+    }
+})
